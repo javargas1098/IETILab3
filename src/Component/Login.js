70a0edd57f1d48ffd21e4d23e5911073c1e2e48c
Javier Vargas <ja.vargas1098@gmail.com>
2019-09-18 23:42:42 -0500

    email

--- src/Component/Login.js
+++ src/Component/Login.js
@@ -6,36 +6,36 @@ import Typography from '@material-ui/core/Typography';
 import Paper from '@material-ui/core/Paper';
 import './Login.css'
 //import {logo} from '../logo.svg';
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import CircularProgress from "@material-ui/core/CircularProgress";
 
 export class Login extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { email: "", password: "", loading:false};
+        this.state = { email: "", password: "", loading: false };
         this.handleEmailSubmit = this.handleEmailSubmit.bind(this);
         this.handlePassSubmit = this.handlePassSubmit.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
-        
+
 
     }
     handleSubmit(e) {
         e.preventDefault();
-        this.setState({loading:true});
-        if (localStorage.getItem('emailDefault') === this.state.email && localStorage.getItem('passwordDefault') === this.state.password){
+        this.setState({ loading: true });
+        if (localStorage.getItem('emailDefault') === this.state.email && localStorage.getItem('passwordDefault') === this.state.password) {
             console.log('entrooooo');
-            localStorage.setItem('page', "Logged");  
-            this.props.reloadPage(); 
-        } 
-        this.setState({ email: "", password: "" ,errorMessage:"Algo Fallo",loading:false});   
-        
+            localStorage.setItem('page', "Logged");
+            this.props.reloadPage();
+        }
+        this.setState({ email: "", password: "", errorMessage: "Algo Fallo", loading: false });
+
     }
-    handlePassSubmit(event) {  
-        this.setState({password: event.target.value});
+    handlePassSubmit(event) {
+        this.setState({ password: event.target.value });
     }
-     handleEmailSubmit(event) {
-        
-        this.setState({email: event.target.value});
+    handleEmailSubmit(event) {
+
+        this.setState({ email: event.target.value });
     }
 
 
@@ -43,7 +43,7 @@ export class Login extends React.Component {
         return (
             <React.Fragment>
 
-                <CssBaseline/>
+                <CssBaseline />
                 <main className="layout">
                     <Paper elevation={5} className="paper">
                         <Typography variant="h4">
@@ -52,18 +52,19 @@ export class Login extends React.Component {
                         {/* <img src={logo} className="App-logo" alt="logo"/> */}
                         <Typography color="error" gutterBottom>{this.state.errorMessage}</Typography>
                         <form className="form" onSubmit={this.handleSubmit}>
+                            <h5>email Javier password ieti</h5>
                             <TextField required label="Username" fullWidth value={this.state.email}
-                                       onChange={this.handleEmailSubmit}/>
+                                onChange={this.handleEmailSubmit} />
                             <TextField required label="Password" type="password" fullWidth value={this.state.password}
-                                       onChange={this.handlePassSubmit}/>
-                            <br/><br/>
+                                onChange={this.handlePassSubmit} />
+                            <br /><br />
                             <Button type="submit" color="primary" variant="contained" fullWidth disabled={this.state.loading}>
                                 Login
                             </Button>
                         </form>
-                        <br/>
+                        <br />
                         <Link>Create account</Link>
-                        {this.state.loading && <CircularProgress style={{marginTop:"6%"}}/>}
+                        {this.state.loading && <CircularProgress style={{ marginTop: "6%" }} />}
                     </Paper>
                 </main>
             </React.Fragment>
